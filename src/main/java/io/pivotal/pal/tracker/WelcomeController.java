6e45f12704737643cda79ecba474c28e430e44ac
Nimish Nasit <nimish192@gmail.com>
2020-01-08 07:08:29 -0700

    Spring Boot Example

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,23 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-    @GetMapping
-    public String sayHello(){
-        return "hello";
+    String welcomeMsg;
+
+    public WelcomeController(
+            @Value("${welcome.message}") String welcomeMsg
+    ){
+        this.welcomeMsg = welcomeMsg;
     }
+
+    @GetMapping("/")
+    public String sayHello(){
+        return welcomeMsg;
+    }
+
 }
